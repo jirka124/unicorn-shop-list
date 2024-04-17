28701f4751f64d52df07286ff30836cd45eac8c1
Jiří Žák <jiri.zak89@seznam.cz>
2024-04-17 14:00:04 +0200

    Added PM2 ecosystem config for change watching
    commit includes:
      - basic pm2 config file allowing live-reload

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "unicorn-shop-list-api",
+      script: "./bin/www",
+      watch: ["./"],
+      watch_delay: 400,
+      ignore_watch: ["node_modules"],
+      wait_ready: true,
+      shutdown_with_message: true,
+      watch_options: {
+        followSymlinks: false,
+      },
+      env_development: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
